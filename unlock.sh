010209b474746593612d51e366ad8623e8458fdf
Rakesh Chowdary <98610361+nvrakesh17@users.noreply.github.com>
2025-01-31 19:31:55 +0530

    Create unlock.sh
    To unlock state file

---
+++ unlock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PROJECT_ID="your-gcp-project-id"
+COLLECTION="terraform-lock"
+DOCUMENT="global"
+
+# Release lock
+gcloud firestore documents update $DOCUMENT --project=$PROJECT_ID --collection=$COLLECTION --update="locked=false"
